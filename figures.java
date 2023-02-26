05c885d8b7d3904bdd64b3666626b19428c68d48
yana <sirazieva..yana@inbox.ru>
2023-02-26 20:26:44 +0300

    end

---
+++ figures.java
@@ -0,0 +1,24 @@
+package java_stepik;
+
+public class figures {
+}
+class Triangle extends figures{
+}
+class RightTriangle extends Triangle{
+}
+class IsoscelesTriangle extends Triangle{
+}
+class EquilateralTriangle extends Triangle{
+}
+class Quadrangle extends figures {
+}
+class Rectangle extends Quadrangle{
+}
+class Rhombus extends Quadrangle{
+}
+class Parallelogram extends Quadrangle{
+}
+class Hexagon extends figures {
+}
+class oval extends figures{}
+class krug extends oval{}
