b1d0a391744d418b0c9c6e371adc3919d3a3670d
yana <sirazieva..yana@inbox.ru>
2023-02-26 23:28:55 +0300

    end

---
+++ transport.java
@@ -0,0 +1,32 @@
+package java_stepik;
+
+public class transport {
+}
+interface Military{
+
+}
+
+interface PublicTransport{
+
+}
+class Train extends transport implements PublicTransport{
+
+}
+class Tank extends transport implements Military{
+
+}
+class Bus extends transport implements PublicTransport{
+
+}
+class Samokat extends transport implements PublicTransport{
+
+}
+class car extends transport implements PublicTransport{
+
+}
+class motobike extends transport implements PublicTransport{
+
+}
+class bycicle extends transport implements PublicTransport{
+
+}
